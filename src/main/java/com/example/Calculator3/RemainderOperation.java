5fc11a068721ca96774e022e98a0c0688947c82a
queenriwon <queenriwon3@gmail.com>
2025-01-09 19:13:44 +0900

    complete

--- src/main/java/com/example/Calculator3/RemainderOperation.java
+++ src/main/java/com/example/Calculator3/RemainderOperation.java
@@ -4,6 +4,7 @@ package com.example.Calculator3;
 public class RemainderOperation extends AbstractOperation {
     @Override
     double operate(Number first, Number second) {
+        if(second.equals(0)) throw new printHowException("[오류] 0으로 나눌 수 없습니다.");
         return first.doubleValue() % second.doubleValue();
     }
 }
